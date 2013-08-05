fff1e481c6c29c12fac07e4944a61e03214b8df2
Ayhan AKTAS <akuma@xskip.net>
2013-08-05 22:16:34 +0300

    SQL : Otomatik Üyelik...

--- sql/changes/30 - Auto Registration.sql
+++ sql/changes/30 - Auto Registration.sql
@@ -1,23 +1,16 @@
-ALTER PROCEDURE [dbo].[ACCOUNT_LOGOUT]
-  @AccountID	varchar(21),
-	@nRet		smallint OUTPUT
-AS
-BEGIN
-	DELETE FROM CURRENTUSER WHERE strAccountID = @AccountID
-	SET @nRet = 1
-END
-GO
-CREATE PROCEDURE [dbo].[MAIN_LOGIN]
+ALTER PROCEDURE [dbo].[MAIN_LOGIN]
         @AccountID      varchar(21),
         @Password       varchar(13),
         @nRet smallint OUTPUT
 AS
 DECLARE @pwd varchar(32), @strAuthority tinyint, @expiryTime datetime, @strUserId varchar(21)
 BEGIN
+
         SELECT @pwd = strPasswd, @strAuthority = strAuthority FROM TB_USER WHERE strAccountID = @AccountID
         IF @@ROWCOUNT = 0 -- auto-registration, easiest for testing because of encryption scheme
         BEGIN
-                INSERT INTO TB_USER (strAccountID, strPasswd) VALUES(@AccountID, @Password)
+                INSERT INTO TB_USER (strAccountID, strPasswd,strSealPassword,strAuthority,PremiumType,PremiumStartDate,PremiumEndDate) VALUES(@AccountID, @Password,12345678,1,7,GETDATE(),GETDATE()+360)
+				DELETE FROM CURRENTUSER WHERE strAccountID = @AccountID
                 SET @nRet = 1 --2
                 RETURN
         END
@@ -33,6 +26,9 @@ BEGIN
                 SET @nRet = 4
                 RETURN
         END
-        SET @nRet = 1
+		DELETE FROM CURRENTUSER WHERE strAccountID = @AccountID
+		SET @nRet = 1
+		RETURN
+		
 END
 GO
