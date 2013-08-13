49c8b322bb3c58a1b50a11dedb4963606ee95c29
AKUMA <akuma@xskip.net>
2013-08-13 04:09:16 -0700

    Game Server : Patlamaya Sebep Olan Otomatik NP Bağışındaki Sorun Düzeltildi...

--- src/GameServer/User.cpp
+++ src/GameServer/User.cpp
@@ -610,25 +610,27 @@ void CUser::SendLoyaltyChange(int32 nChangeAmount /*= 0*/, bool bIsKillReward /*
 		{
 			CKnights * pKnights = g_pMain->GetClanPtr(GetClanID());
 
-			if (pKnights->m_byFlag >= ClanTypeAccredited5)
-			{
-				if (pKnights->m_sMembers <= 5)
-					nClanLoyaltyAmount = 1;
-				else if (pKnights->m_sMembers <= 10)
-					nClanLoyaltyAmount = 2;
-				else if (pKnights->m_sMembers <= 15)
-					nClanLoyaltyAmount = 3;
-				else if (pKnights->m_sMembers <= 20)
-					nClanLoyaltyAmount = 4;
-				else if (pKnights->m_sMembers <= 25)
-					nClanLoyaltyAmount = 5;
-				else if (pKnights->m_sMembers <= 30)
-					nClanLoyaltyAmount = 6;
-				else if (pKnights->m_sMembers > 30)
-					nClanLoyaltyAmount = 6;
+			if (pKnights != nullptr) {
+				if (pKnights->m_byFlag >= ClanTypeAccredited5)
+				{
+					if (pKnights->m_sMembers <= 5)
+						nClanLoyaltyAmount = 1;
+					else if (pKnights->m_sMembers <= 10)
+						nClanLoyaltyAmount = 2;
+					else if (pKnights->m_sMembers <= 15)
+						nClanLoyaltyAmount = 3;
+					else if (pKnights->m_sMembers <= 20)
+						nClanLoyaltyAmount = 4;
+					else if (pKnights->m_sMembers <= 25)
+						nClanLoyaltyAmount = 5;
+					else if (pKnights->m_sMembers <= 30)
+						nClanLoyaltyAmount = 6;
+					else if (pKnights->m_sMembers > 30)
+						nClanLoyaltyAmount = 6;
 
-				m_iLoyalty -= nClanLoyaltyAmount;
-				CKnightsManager::AddUserDonatedNP(GetClanID(),m_strUserID,nClanLoyaltyAmount,true);
+					m_iLoyalty -= nClanLoyaltyAmount;
+					CKnightsManager::AddUserDonatedNP(GetClanID(),m_strUserID,nClanLoyaltyAmount,true);
+				}
 			}
 		}
 	}
